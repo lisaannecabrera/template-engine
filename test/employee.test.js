beb5a6c140a12eaf3561f86a21042b09e0eb169e
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-02-03 17:38:41 -0800

    Employee name test passed

--- test/employee.test.js
+++ test/employee.test.js
@@ -3,13 +3,21 @@
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
+  let employee;
+
+  beforeEach(() => {
+    employee = new Employee(10, "lisa");
+  });
   describe("initialization", () => {
     it("should create an object with an id property", () => {
       // arrange and act
-      const employee = new Employee(10);
+      //   const employee = new Employee(10);
 
       // assert
       expect(employee.id).toEqual(10);
+    });
+    it("should creat an object with a name property", () => {
+      expect(employee.name).toEqual("lisa");
     });
   });
 });
