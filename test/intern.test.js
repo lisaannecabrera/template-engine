fc7224497f9faba7d6040b273ce0cb52151acbf5
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-02-04 20:37:25 -0800

    made intern.js and tests passed

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,3 +1,26 @@
 "use strict";
 
-const Intern = require("../lib/employee");
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+  let intern;
+
+  beforeEach(() => {
+    intern = new Intern("IUPUI");
+  });
+  describe("initialization", () => {
+    it("should create an object with the Intern's school", () => {
+      expect(intern.internSchool).toEqual("IUPUI");
+    });
+  });
+  describe("getSchool", () => {
+    it("should return the school of the Intern", () => {
+      expect(intern.getSchool()).toEqual("IUPUI");
+    });
+  });
+  describe("getRole", () => {
+    it("should return the role Intern", () => {
+      expect(intern.getRole()).toEqual("Intern");
+    });
+  });
+});
